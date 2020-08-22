3e360db3e20fdf266b6d22c7dd94f0512aba9eac
egi <daegi01@gmail.com>
2020-08-23 01:31:35 +0200

    bump version

--- Stats/Mod.cs
+++ Stats/Mod.cs
@@ -28,7 +28,7 @@ namespace Stats
         public string SystemName => "Stats";
         public string Name => "Stats";
         public string Description => "Adds a configurable panel to display all vital city stats at a glance.";
-        public string Version => "1.2.0";
+        public string Version => "1.3.0";
         public string WorkshopId => "1410077595";
 
         public void OnEnabled()
