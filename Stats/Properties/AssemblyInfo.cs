763e310deba47ce57f89f1ea258db14b38024ec0
egi <daegi01@gmail.com>
2021-06-23 22:42:46 +0200

    Updated csproj files to SDK format.

--- Stats/Properties/AssemblyInfo.cs
+++ Stats/Properties/AssemblyInfo.cs
@@ -1,17 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Stats")]
-[assembly: AssemblyDescription("Adds a configurable panel to display all vital city stats at a glance.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Egi")]
-[assembly: AssemblyProduct("Stats")]
-[assembly: AssemblyCopyright("MIT")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-
 [assembly: Guid("ac1006d3-4120-4a40-8bc6-38d168b8a2f0")]
-
-[assembly: AssemblyVersion("1.0.*")]
